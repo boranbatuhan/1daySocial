f41c0e46af5942fb2ff67ce56d36118b312dd829
boranbatuhan <boranbatuh@gmail.com>
2023-11-03 02:03:54 +0300

    timer logic

--- components/post.vue
+++ components/post.vue
@@ -1,21 +1,29 @@
 
 <template>
     <p class="text-3xl">{{props.post.id}}</p>
+    <p class="text-3xl">{{(post.timer/1000).toFixed(0)}}</p>
+    
     <div class=" border rounded-lg max-w-lg relative" :class="user.theme">
+        <p class="absolute top-1 left-3 text-xs select-none">{{dateFormat(props.post.date)}}</p>
     <div class="absolute top-1 right-1">
         <div @click="fireBtn(props.post.id)" class="relative group w-fit h-fit  cursor-pointer" >
             <span  class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-rose-600 group-hover:w-6 w-0 group-hover:h-6 h-0 blur-md  transition-all"></span>
             <Icon :name="post.vote.isFire ==true ? 'basil:fire-solid' : 'basil:fire-outline'" size="2rem" :class="{'!text-rose-600': post.vote.isFire==true }" class="relative text-stone-50 group-hover:saturate-200 group-hover:text-rose-600" />
         </div>
     </div>
-        <div class="p-3">
-            <p class="before:content-['@'] select-none cursor-pointer w-fit">{{props.post.author}}</p>
+        <div class="pb-3 pt-6 px-6">
+            <p class="before:content-['@'] select-none cursor-pointer w-fit hover:underline">{{props.post.author}}</p>
             <p class="!text-white text-justify py-2">{{props.post.content}}</p>
         </div>
 
-    <div class="w-full h-1 mt-2 bg-white/50 relative overflow-hidden group ">
-        <div class="w-full h-1 bg-green-500 group-hover:bg-red-500 saturate-200 right-[0%] group-hover:right-[100%] absolute transition-all duration-1000" >
+        
+        <div class="w-full h-fit mt-2 p-0  overflow-hidden group flex items-end justify-between " :class="user.theme">
+        <p :class="{'opacity-50':post.timer<=0}" class=" text-xs select-none ml-3">{{dateFormat(props.post.date)}}</p>
+        <p :class="{'opacity-50':post.timer>0}"  class=" text-xs select-none ml-3">{{dateFormat(props.post.finaldate)}}</p>
+            
         </div>
+        <div class="w-full h-fit mt-2 p-0  overflow-hidden group flex items-start justify-start " :class="user.theme">
+        <input type="range" class="w-full h-2 m-0 p-0 select-none pointer-events-none outline-none border-0 ring-0" :min="props.post.date" :max="props.post.finaldate" :value="props.post.finaldate - post.timer">
     </div>
     <ul class="flex gap-4  p-2  w-full" :class="user.theme" >
         <li class="flex gap-1 items-center justify-start">
@@ -50,8 +58,11 @@ const post = reactive({
         isVote:false,
 
         isFire:false
-    }
+    },
+    timer:0,
+    timeLeft:props.post.finaldate - Date.now()
 })
+
 
 const fireBtn =(postid)=>{
     if(post.vote.isFire)
@@ -69,4 +80,114 @@ const dislikeBtn =(postid)=>{
     postsStore.postDislike(postid,user.userid)
 }
 
+
+const dateFormat = (time)=>{
+    // 3m 600k =1saat
+    var date = new Date(time)
+    
+    var fullDate = date.getDate()+
+    "/"+(date.getMonth()+1)+
+    "/"+date.getFullYear()+
+    " "+date.getHours()+
+    ":"+date.getMinutes()
+    
+    
+    return  fullDate
+}
+const timer = setInterval(() => {
+    post.timer=props.post.finaldate - Date.now()
+    if(props.post.finaldate - Date.now() <=0)
+    {
+        post.timer=0
+        clearInterval(timer)
+    }
+}, 1000);
+
 </script>
+<style>
+input[type=range] {
+  -webkit-appearance: none;
+  margin: 10px 0;
+  width: 100%;
+}
+input[type=range]:focus {
+  outline: none;
+}
+input[type=range]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 8px;
+  cursor: pointer;
+  animate: 0.2s;
+  box-shadow: 0px 0px 0px #000000;
+  background: rgb(9, 9, 11);
+  border-radius: 0px;
+  border: 0px solid #000000;
+}
+input[type=range]::-webkit-slider-thumb {
+  box-shadow: 0px 0px 1px rgb(9, 9, 11);
+  border: 0px solid rgb(9, 9, 11);
+  height: 8px;
+  width: 8px;
+  border-radius: 1px;
+  background: #9bfb00;
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: 0px;
+}
+input[type=range]:focus::-webkit-slider-runnable-track {
+  background: rgb(9, 9, 11);
+}
+input[type=range]::-moz-range-track {
+  width: 100%;
+  height: 8px;
+  cursor: pointer;
+  animate: 0.2s;
+  box-shadow: 0px 0px 0px #000000;
+  background: rgb(9, 9, 11);
+  border-radius: 0px;
+  border: 0px solid #000000;
+}
+input[type=range]::-moz-range-thumb {
+  height: 8px;
+  width: 8px;
+  border-radius: 1px;
+  background: #9bfb00;
+  cursor: pointer;
+}
+input[type=range]::-ms-track {
+  width: 100%;
+  height: 8px;
+  cursor: pointer;
+  animate: 0.2s;
+  background: rgb(9, 9, 11);
+  border-color: rgb(9, 9, 11);
+  color: rgb(9, 9, 11);
+}
+input[type=range]::-ms-fill-lower {
+  background: rgb(9, 9, 11);
+  border: 0px solid #000000;
+  border-radius: 0px;
+  box-shadow: 0px 0px 0px #000000;
+}
+input[type=range]::-ms-fill-upper {
+  background: rgb(9, 9, 11);
+  border: 0px solid #000000;
+  border-radius: 0px;
+  box-shadow: 0px 0px 0px #000000;
+}
+input[type=range]::-ms-thumb {
+  box-shadow: 0px 0px 1px rgb(9, 9, 11);
+  border: 0px solid rgb(9, 9, 11);
+  height: 8px;
+  width: 8px;
+  border-radius: 1px;
+  background: #9bfb00;
+  cursor: pointer;
+}
+input[type=range]:focus::-ms-fill-lower {
+  background: rgb(9, 9, 11);
+}
+input[type=range]:focus::-ms-fill-upper {
+  background: rgb(9, 9, 11);
+}
+</style>
