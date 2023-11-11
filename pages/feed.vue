737e503c8931e9d876648238cfc4c641a16442d5
boranbatuhan <boranbatuh@gmail.com>
2023-11-11 16:49:02 +0300

    reload posts

--- pages/feed.vue
+++ pages/feed.vue
@@ -3,11 +3,13 @@
         <ul class="flex flex-col items-center justify-start" v-auto-animate>
             <li class=" !bg-stone-950 z-[99] rounded-lg">
                <new-post/> 
-               <div class="bg-gradient-to-b from-stone-950 to-transparent w-full h-8"></div>
             </li>
             <li class="w-[32rem] h-8 group sticky top-0 z-[99999]"  >
                 <div v-if="y>250" @click="scrollTop" class="bg-stone-950 border-b border-stone-800 group-hover:bg-stone-700 w-full h-8  cursor-pointer flex items-center justify-center">
                     <Icon name="basil:caret-up-solid" size="2rem"/>
+                </div>
+                <div v-if="y<250" @click="reloadPosts" class="bg-stone-950 border-b border-stone-800 group-hover:bg-stone-700 w-full h-8  cursor-pointer flex items-center justify-center">
+                    <Icon name="basil:history-outline" size="2rem"/>
                 </div>
             </li>
             <li v-for="post in posts" :key="post.id" class=" my-2" >
@@ -43,6 +45,11 @@ const scrollTop=()=>{
           top: 0,
           behavior: "smooth"
         });
+}
+
+const reloadPosts=()=>{
+    usePostsStore().setPostsFromDB()
+
 }
 
 </script>
